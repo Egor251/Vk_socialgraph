1b7a7b03706a16463e860fdc8bd2034f70364c5e
Egor251 <grimmallod@mail.ru>
2024-07-06 23:32:37 +0300

    benchmark decorator added

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@
 from numba import njit
 from tqdm import tqdm
 import os
+import time
 
 
 class Utils:
@@ -29,3 +30,23 @@ class Utils:
     def set_unique(list1):
         return list(set(list1))
 
+
+    @staticmethod
+    def benchmark(func):  # Декоратор для вычисления времени работы функции. полезен при оптимизации
+        # Вызывать @Utils().benchmark
+        def calc_time(*args, **kwargs):
+            start_time = time.time()
+            original_result = func(*args, **kwargs)
+            full_time = time.time() - start_time
+            print(f'Function {func.__name__}() finished in {full_time} sec')
+            return original_result
+        return calc_time
+
+
+if __name__ == '__main__':
+
+    @Utils().benchmark
+    def test():
+        original_list = list(range(1000000))
+
+    test()
